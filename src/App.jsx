da75f568629625fd7864b3a4e0cff0f94cc14a29
delacroix <delacroix.jon@gmail.com>
2025-06-23 15:24:19 +0200

    Activité 4

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,34 @@ function App() {
     fetchProducts();
   }, []);
 
+  const handleAddProduct = async () => {
+    const response = await fetch('https://fakestoreapi.com/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: 'Ordinateur Apple',
+        price: 8.99,
+        description: 'Un ordianteur de la marque APPLE',
+        image: 'https://cdn.pixabay.com/photo/2016/11/29/08/41/apple-1868496_1280.jpg',
+        category: 'pc',
+      }),
+    });
+
+    const data = await response.json();
+
+    alert(`Le produit avec l'id ${data.id} a été créé`);
+  };
+
+
   return (
     <Container className="mt-4">
+  
+      <button onClick={handleAddProduct} className="mb-4">
+        Ajouter un produit
+      </button>
+
       <Row className="gy-3">
         {products.map(p => (
           <Col key={p.id} md={3}>
