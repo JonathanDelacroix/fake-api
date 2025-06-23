b3308f36c7f525a28fdae522f73bcb4349935bb6
delacroix <delacroix.jon@gmail.com>
2025-06-23 12:01:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+
+  // const response = await fetch ("https://fakestoreapi.com/products");
+  // const products = await response.json();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    }
+
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        {products.map(p => (
+          <Col key={p.id} md={3} className="mb-4 d-flex flex-column">
+            <Card className="flex-grow-1">
+              <Card.Img
+                variant="top"
+                src={p.image}
+                style={{ height: '200px', objectFit: 'contain' }}
+              />
+              <Card.Body>
+                <Card.Title>{p.title}</Card.Title>
+                <Card.Text>{p.description} €</Card.Text>
+                <Card.Text>{p.price} €</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
